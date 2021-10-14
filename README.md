8e024c724d417173099f549c1234f6970eede1b5
bengetch <bengetch@gmail.com>
2021-10-14 13:11:55 -0400

    more typo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# fsu
+# sfu
 Snowflake URI utility library that supports extraction of Snowflake configuration data and method parameters from Snowflake resource URIs.
 
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="sfu",
     version="1.0.0",
-    packages=["sfu",],
+    packages=["sfu"],
     install_requires=[],
     license="MIT",
     url="https://github.com/nthparty/sfu",
