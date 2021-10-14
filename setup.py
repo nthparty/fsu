51e6e10f66ad6eda211764bb1b362643f87872c6
bengetch <bengetch@gmail.com>
2021-10-14 13:09:36 -0400

    typo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="b3u",
+    name="sfu",
     version="1.0.0",
     packages=["sfu",],
     install_requires=[],
